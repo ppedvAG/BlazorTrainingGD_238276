9e47549fdef144c46d5c1ac8f9bd932cbb3fbde6
Hannes Preishuber <hannesp@ppedv.de>
2023-10-20 10:17:03 +0200

    Fix für Validation CSS ala Bootstrap

---
+++ BlazorTrainingGD_238276/Pages/modul5/BootstrapFixFieldCssClassProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorTrainingGD_238276.Pages.modul5
+{
+    public class BootstrapFixFieldCssClassProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+            bool IsValid=!editContext.GetValidationMessages(fieldIdentifier).Any();
+
+            return IsValid ? "is-valid" : "is-invalid";
+        }
+    }
+}
