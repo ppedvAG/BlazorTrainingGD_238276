ed5131b2d6156785ce977d9b63ed25eb8756fbcc
Hannes Preishuber <hannesp@ppedv.de>
2023-10-20 10:07:01 +0200

    Formular eigener Text Input Component

---
+++ BlazorTrainingGD_238276/Pages/modul5/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTrainingGD_238276.Pages.modul5
+{
+    public class Person
+    {
+        [Required(ErrorMessage ="da muss was rein")]
+        [StringLength(10,ErrorMessage ="max 10 Zeichen")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "da muss was rein")]
+        [Range(150,180,ErrorMessage ="zwischne 150 und 180")]
+        public int Gross { get; set; }
+        [Required(ErrorMessage = "da muss was rein")]
+        public DateTime GebDat { get; set; }
+
+    }
+}
